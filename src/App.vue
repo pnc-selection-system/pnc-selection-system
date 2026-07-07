40f174b2b51714f47b5e25aacc78f41bffb0c63d
sokkeangYeang <sokkeang168861@gmail.com>
2026-07-07 11:32:07 +0700

    feat: AdminLayout(#FE-06)

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,29 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import AdminLayout from './layouts/AdminLayout.vue'
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <AdminLayout>
+    <h1>You did it!</h1>
+    <p>
+      Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+      documentation
+    </p>
+  </AdminLayout>
 </template>
 
-<style scoped></style>
+<style scoped>
+h1 {
+  font-size: 24px;
+  margin-bottom: 16px;
+}
+
+a {
+  color: #3b82f6;
+  text-decoration: none;
+}
+
+a:hover {
+  text-decoration: underline;
+}
+</style>
